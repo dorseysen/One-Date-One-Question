5804afaaa8ebd17b737061ca74010ff6f59c5ff3
dorseysen <1092240376@qq.com>
2019-05-06 10:05:46 +0800

    初次提交

---
+++ 2019/2019-04.js
@@ -0,0 +1,94 @@
+// 每天做一道题。
+
+// 2019-04-28：[1, 2, 3, 50, 88, 65, 4, 89, 1111]，输出[88, 89, 1111]。 （大于80的所有数），至少两种解法。
+
+// the first solution
+console.log([1, 2, 3, 50, 88, 65, 4, 89, 1111].filter(item => item > 80));
+
+//  the second solution
+let res = [];
+[1, 2, 3, 50, 88, 65, 4, 89, 1111].forEach(item => {
+    if(item > 80) {
+        res.push(item);
+    }
+});
+console.log(res);
+
+//  this third solution is similar to the first one does not use the filter.
+const handle20190429 = ( arr, condition ) => {
+
+    let res = [];
+    arr.forEach(item => {
+        if(condition(item)) {
+            res.push(item);
+        }
+    });
+    return res;
+}
+console.log(handle20190429([1, 2, 3, 50, 88, 65, 4, 89, 1111], item => item > 80));
+
+//  2019-04-29： 不通过sort方法，对任意数组（成员都是整数）进行排序，如[9, -2, 200, 35]，输出：[-2, 9, 35, 200];
+
+//  冒泡排序
+
+const swap = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]];
+
+const sort20190430 = arr => {
+
+    for(let i = 0; i < arr.length - 1; i ++) {
+
+        for(let j = arr.length; j > i; j --) {
+            
+            if(arr[j - 1] > arr[j]) {
+
+                swap(arr, j - 1, j);
+            }
+        }
+    }
+    return arr;
+}
+console.log(sort20190430([1, 2, 3, 50, -88, 65, -4, 89, 1111]));
+
+//  快排
+const quickSort20190430 = arr => {
+
+    const quickUnit = (arr, left, right) => {
+
+        let i = left,
+            j = right,
+            reference = arr[left];
+            if(i >= j) return;      //当左右指针碰撞时直接跳出执行逻辑
+            while (i < j) {
+
+                while (i < j && arr[j] >= reference) j --;
+
+                while (i < j && arr[i] <= reference) i ++;
+
+                if(arr[i] > arr[j]) swap(arr, i, j);
+
+            }
+            swap(arr, left, i);
+            quickUnit(arr, left, i - 1);
+            quickUnit(arr, i + 1, right);
+    }
+    quickUnit(arr, 0, arr.length - 1);
+    return arr;
+}
+
+console.log(quickSort20190430([1, 2, 3, 50, -88, 65, -4, 89, 1111]));
+
+//  2019-04-30： 有数组：[1, 2, 100, 80, 33, 90] 和 [-8, 77, 90, 34, 56, 1179]，求中位数。
+
+//  基本思路是合并数组，排序再取中间值，该方法性能不足，但较为直观。
+const median20190430 = ( arr0, arr1 ) => {
+
+    let arr = arr0.concat( arr1 );
+
+    arr.sort( ( a, b ) => a - b );
+
+    return arr.length % 2 ? arr[( arr.length - 1 ) / 2] : ( arr[( arr.length / 2 ) - 1] + arr[ arr.length / 2] ) / 2;
+}
+console.log(median20190430([1, 2], [-8, -2]));
+
+
+
---
+++ 2019/2019-05.js
@@ -0,0 +1,48 @@
+//  2019-05-05： 请写一个函数，用于判断类型。比如[1]返回array，1返回number，'1'返回string。
+
+//  the first solution
+
+const getType_20190505_0 = obj => {
+
+    let typeStr = Object.prototype.toString.call(obj);
+
+    return typeStr.split(' ')[1].split(']')[0].toLowerCase();
+
+}
+console.log(getType_20190505_0([1]));
+console.log(getType_20190505_0(1));
+console.log(getType_20190505_0('1'));
+
+//  the second solution
+const getType_20190505 = obj => Object.prototype.toString.call(obj).match(/(?<=\s).*?(?=\])/g)[0].toLowerCase();
+
+console.log(getType_20190505([1]));
+console.log(getType_20190505(1));
+console.log(getType_20190505('1'));
+
+// 2019-05-06： 有JSON数据如下：
+// [
+//     {
+//         TIME: '2019-03-02 23:16:08',
+//         records: 'xiaoming'
+//     },
+//     {
+//         TIME: '2019-06-05 20:16:08',
+//         records: 'xiaohua'
+//     },
+//     {
+//         TIME: '2019-03-02 23:16:10',
+//         records: 'xiaohong'
+//     },
+// ]
+// 请对该数组按时间进行排序。
+
+
+//  2019-05-07:  当a的值在什么的情况下，可以使下列式子打印出1：
+
+if (a == 1 && a == 2 && a == 3) {
+    console.log(1);
+}
+
+
+//  2019-05-08： 有字符串'dhaoshdaoidhadia'，打印出最长不重复的字符串片段。
