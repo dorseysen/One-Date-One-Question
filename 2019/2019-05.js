f6a313d7fd37d667c1e8cc827967569bafe6a590
dorseysen <1092240376@qq.com>
2019-05-20 10:36:58 +0800

    2019-05-20：今天是520，随意说说用JavaScript表达爱意的一种方式，最好是贴出你的代码。

--- 2019/2019-05.js
+++ 2019/2019-05.js
@@ -230,3 +230,30 @@ const sliceArr_20190515 = (arr, size) => {
 console.log(sliceArr_20190515([2, 3, 4, 5, 6, 7, 8, 9, 0, 10], 3));
 
 //  2019-05-16：判断一个字符串是否是回文字符串
+
+const palindromeString_20190516 = str => str.split('').reverse().join('') === str;
+
+console.log(palindromeString_20190516('aba'));
+
+//  2019-05-17：写一个简单的数据深拷贝，纯数据
+
+const deepCopy_20190517 = obj => JSON.parse(JSON.stringify(obj));
+
+console.log(deepCopy_20190517({ name: 'dorsey', age: '25' }));
+
+//  2019-05-18：解释一下什么是短路、断路操作符
+
+// 短路断路操作符其实就是利用 &&/||/! 等逻辑运算符的熔断机制，达到相应的判断目的。这是一个较好的替代if/else机制的方案。
+// 如：
+
+const shortout_20190518 = res => res && res.length && res.map( (item, index) => { index: item } );
+
+//  这里就是运用短路操作符做判空处理，避免报空指针错误。
+
+//  2019-05-19：JavaScript写一个获取某范围内随机整数的函数，可随意改变这个范围
+
+const randomInt_20190519 = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+//  2019-05-20：今天是520，随意说说用JavaScript表达爱意的一种方式，最好是贴出你的代码。
+
+
