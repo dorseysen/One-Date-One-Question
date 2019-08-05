5f502e58aaea61170e741af8fe18715d82cdf1c7
dorseysen <1092240376@qq.com>
2019-08-05 21:26:16 +0800

    2019-08-05  合并多个有序数组并去重，合并数组从小到大排序

--- src/2019/2019-07.js
+++ src/2019/2019-07.js
@@ -85,28 +85,101 @@ export const solution_201907 = {
     },
     "2019-07-04" () {
 
-        //  BFS
-        return "2019-07-04";
+        // 2019-07-04：数组去重，且该数组已排序，不能用Set集合，且不能新开内存，只能原引用做操作
+
+        const removeDuplicatesArray = nums => {
+
+            for(let i = 1; i < nums.length; i ++) {
+        
+                if(nums[i] === nums[i - 1]) {
+                    nums.splice(i, 1);
+                    i --;
+                }
+            }
+            return nums;
+        }
+        return removeDuplicatesArray([1, 1, 2]);
     },
     "2019-07-05" () {
 
-        //  DFS
+        // 2019-07-05：输入某个大于0的整数，输出从1开始的连续奇数乘积，
 
-        return "2019-07-05";  
+        // 当乘积结果刚好大于或等于输入值时，输出这条式子
+
+        const oddMultiply = num => {
+
+            let i = 1, multiply = 1, res = [1];
+
+            while(multiply < num) {
+
+                multiply *= i += 2;
+
+                res.push(i);
+            }
+
+            res = res.join(' × ') + ' = ' + multiply;
+
+            multiply > num ? res += ' > ' + num : res += ' = ' + num;
+
+            return res;
+        }
+
+        return oddMultiply(1000);  
     },
     "2019-07-06" () {
+        //  2019-07-06：写一个函数，用于输出未来三个月的时间范围。
 
-        //  2019-07-30：sku算法———多维属性状态判断
-        //  算法简化：假设只有3种状态：1、颜色：红蓝灰。2、尺码、大中小。型号、ABC。
-        //  此时库存只有以下数据源：
-        // [
-        //     { "颜色": "红", "尺码": "大", "型号": "A", "skuId": "3158054" },
-        //     { "颜色": "白", "尺码": "中", "型号": "B", "skuId": "3133859" },
-        //     { "颜色": "蓝", "尺码": "小", "型号": "C", "skuId": "3516833" }
-        //  ]
-        // 数据源不可选时将选项按钮置灰并禁用点击，请写出该SKU算法。
+        //  比如今天是2019-07-08 则输出： 
 
-        return "2019-07-06";
+        // {
+        //     beginTime: '2019-07-08 09:26:14',
+        //     endTime: '2019-10-08 09:26:14'
+        // }
+
+        //  需注意隔年问题
+
+        class futureMonths {
+
+            constructor (num, format) {
+
+                this.format = format;
+
+                this.res = [];
+
+                this.init( num );
+
+            }
+            init ( num ) {
+
+                let date = new Date();
+
+                this.res.push( this.dateFormat(date) );
+
+                //  核心代码 —— 未来几个月
+
+                let month = date.getMonth() + 1 + num % 12,
+                    year = date.getFullYear() + Math.floor( num / 12 );
+
+                month = month > 12 ? ( year ++, month % 12 ) : month;
+
+                this.res.push( this.dateFormat(date, [year, month]) );
+            }
+
+            dateFormat ( time, partTime ) {
+
+                return this.format.replace('yyyy', partTime ? partTime[0] : time.getFullYear())
+                                .replace('MM', this.fullFormat(partTime ? partTime[1] : time.getMonth() + 1))
+                                .replace('dd', this.fullFormat(time.getDate()))
+                                .replace('HH', this.fullFormat(time.getHours()))
+                                .replace('mm', this.fullFormat(time.getMinutes()))
+                                .replace('ss', this.fullFormat(time.getSeconds()));
+            }
+            fullFormat ( num ) {
+
+                return num >= 10 ? num : '0' + num;
+            }
+        }
+        return new futureMonths(3, 'yyyy-MM-dd HH:mm:ss');
     },
     "2019-07-07" () {
 
@@ -431,7 +504,7 @@ export const solution_201907 = {
     },
     "2019-07-19" () {
 
-        //  2019-07-18：给定字符串str，检查其是否存在连续3个升序数字。
+        //  2019-07-19：给定字符串str，检查其是否存在连续3个升序数字。
         //  如果有，返回第一个出现的连续3个数字（或者最后一次出现的连续三个数字）
         //  如果没有，返回不存在
 
@@ -464,68 +537,302 @@ export const solution_201907 = {
 
     },
     "2019-07-20" () {
+        //  2019-07-20  输入的大数组中包含了4个小数组，分别找到每个小数组中的最大值，然后把它们串联起来，形成一个新数组。
+        //  如输入[[1,2,3], [4,5,6], [7,8], [9,10,11]]，输出[3, 6, 8, 11]
 
+        const arrChange = arr => arr.map(item => Math.max.apply(-Infinity, item));
+
+        return arrChange([[1,2,3], [4,5,6], [7,8], [9,10,11]]);
     },
     "2019-07-21" () {
 
+        //  2019-07-21  实现一个摧毁(destroyer)函数，第一个参数是待摧毁的数组，其余的参数是待摧毁的值。
+        //  如输入[1,2,3,4,5,6,7,8,9] 和 2, 5, 8， 输出 [1,3,4,6,7,9]
+
+
+        function destroyer () {
+
+            let argu = Array.from(arguments);
+
+            argu.splice(0, 1);
+
+            return arguments[0].filter(item => argu.indexOf(item) === -1);
+        }
+
+        return destroyer([1,2,3,4,5,6,7,8,9,8,5,2,2,4,5,6], 2, 5, 8);
+
     },
     "2019-07-22" () {
+        //  2019-07-22  模拟抽奖，抽奖的号码为员工号，抽过的员工不能再次出现在抽奖号码中
 
-    },
-    "2019-07-23" () {
-        //  2019-07-32：交叉组合
+        class lottery {
 
-        // 有一个数组，如果有3个值：[3,2,6]。交叉组合后返回：3-2，3-6，2-6，3-2-6
+            constructor (arr, times) {
 
-        // 有一个数组，如果有4个值：[3,2,6,9]。交叉组合后返回：3-2，3-6，3-9，2-6，2-9，6-9，3-2-6，3-2-9，2-6-9，3-2-6-9
+                this.arr = arr;
+                this.times = times;
+                this.luckyPerson = [];
+                this.init();
+            }
+            init () {
 
-        // 5个值，6个值以此类推
-    },
-    "2019-07-24" () {
+                while(this.times --) {
 
-        /*  2019-07-19: 子数组最大和
-            比如输入：[1, 2, 3, 4, -5, 6, -7, 8],由于：1 + 2 + 3 + 4 + (-5) + 6 + (-7) + 8 = 12是最大的和。
-            故而输出：[12, [1, 2, 3, 4, -5, 6, -7, 8]]
-            子数组泛指一切由该输入数组任意连续成员所组成的数组，比如[1, 2, 3, 4]是一个子数组，[3, 4, -5]也是子数组, [-7]也是子数组
-            甚至输入数组本身也可以作为它的子数组 */
-            const maximumSum = arr => {
-
-                let median = 0, res;
-    
-                for( let i = 0; i < arr.length; i ++ ) {
-    
-                    
+                    this.luckyPerson.push(...this.lotteryUnit());
                 }
             }
 
+            lotteryUnit () {
+
+                let index = this.createRandomIndex(0, this.arr.length);
+
+                return this.arr.splice(index, 1);
+            }
+            createRandomIndex (min, max) {
+
+                return Math.floor(Math.random() * (max - min) + min);
+            }
+        }
+
+        return new lottery([1,2,3,4,5,6,7,8,9,10], 3);
+    },
+    "2019-07-23" () {
+        
+        //  2019-07-23  获取URL参数，并以json的格式输出。
+        //  如输入：http://www.baidu.com/item.html#hello?pageNo=1&pageSize=30&keyword=hello
+        //  输出：{
+        //      pageNo: '1',
+        //      pageSize: '30',
+        //      keyword: 'hello'
+        //  }
+
+        const getUrlParams = url => {
+
+            let arr = url.match(/(?<=[\?&]).*?(?=(&|$))/g),
+                res = {};
+
+            arr && arr.length && arr.map(item => res[item.split('=')[0]] = item.split('=')[1]);
+
+            return res;
+        }
+        
+        console.log(getUrlParams('http://www.baidu.com'));
+
+        return getUrlParams('http://www.baidu.com/item.html#hello?pageNo=1&pageSize=30&keyword=hello');
+    },
+    "2019-07-24" () {
+        // 2019-07-24 实现一个lazyman函数 
+        //  需具备的功能细节如下：
+        /**   
+            实现一个LazyMan，可以按照以下方式调用:
+            LazyMan(“Hank”)输出:
+            Hi! This is Hank!
+
+            LazyMan(“Hank”).sleep(10).eat(“dinner”)输出
+            Hi! This is Hank!
+            //等待10秒..
+            Wake up after 10
+            Eat dinner~
+
+            LazyMan(“Hank”).eat(“dinner”).eat(“supper”)输出
+            Hi This is Hank!
+            Eat dinner~
+            Eat supper~
+
+            LazyMan(“Hank”).sleepFirst(5).eat(“supper”)输出
+            //等待5秒
+            Wake up after 5
+            Hi This is Hank!
+            Eat supper
+
+            以此类推。
+        */
+       //  这里的核心其实就是实现一个简单的任务消费队列
+
+        class _LazyMan {
+
+            constructor (personName) {
+
+                this.personName = personName;
+                this.task = [];
+                this.init();
+            }
+            init () {
+
+                let _self = this;
+
+                let fn = function () {
+                    console.log('Hi! This is ' + _self.personName + '!');
+                    _self.next();
+                }
+                _self.task.push(fn);
+
+                setTimeout(() => _self.next(), 0);
+            }
+            //  队列指针往右位移一位并删除队列指针前面的已完成任务。
+            next () {
+                let fn = this.task.shift();
+                fn && fn();
+            }
+            eat (meal) {
+                
+                let _self = this;
+                let fn = function () {
+                    console.log('Eat ' + meal + '~');
+                    _self.next();
+                }
+                this.task.push(fn);
+                return this;
+            }
+            sleep (time) {
+
+                let _self = this;
+                let fn = function () {
+                    // console.log('sleep ing...');
+                    setTimeout(() => {
+                        console.log('sleep completed, Wake up after ' + time + ' second');
+                        _self.next();
+                    }, time * 1000);
+                }
+                this.task.push(fn);
+                return this;
+            }
+            sleepFirst (time) {
+
+                let _self = this;
+                let fn = function () {
+                    // console.log('sleepFirst ing...');
+                    setTimeout(() => {
+                        console.log('sleepFirst completed, Wake up after ' + time + ' second');
+                        _self.next();
+                    }, time * 1000);
+                }
+                this.task.unshift(fn);
+                return this;
+            }
+        }
+
+        function LazyMan (personName) {
+
+            return new _LazyMan(personName);
+        }
+        // LazyMan('Hank').sleep(10);
+        // LazyMan("Hank").sleep(5).eat("dinner");
+        LazyMan("dorsey").eat("breakfirst").sleep(2).eat("lunch").sleep(2).eat("dinner").sleepFirst(2);
     },
     "2019-07-25" () {
-        /*实现一个LazyMan，可以按照以下方式调用:
-        
-        LazyMan(“Hank”)输出:
-        Hi! This is Hank!
+        // 2019-07-25 返回一个数组被截断n个元素后还剩余的元素，截断从索引0开始。
 
-        LazyMan(“Hank”).sleep(10).eat(“dinner”)输出
-        Hi! This is Hank!
-        等待10秒..
-        Wake up after 10
-        Eat dinner~
+        const cutArr = (arr, num) => arr.splice(num);
 
-        LazyMan(“Hank”).eat(“dinner”).eat(“supper”)输出
-        Hi This is Hank!
-        Eat dinner~
-        Eat supper~
-
-        LazyMan(“Hank”).sleepFirst(5).eat(“supper”)输出
-        等待5秒
-        Wake up after 5
-        Hi This is Hank!
-        Eat supper
-
-        以此类推。*/
+        return cutArr([1,2,3,4,5], 3);
     },
     "2019-07-26" () {
-        // 2019-07-26：有100格台阶，可以跨1步可以跨2步，那么一个有多少种走法。
-    }
+        //  2019-07-26  计算任意一个数（Number)各个位数之和
 
+        const sumOfDigits = num => {
+            let res = 0;
+            while(num) {
+                res += num % 10;
+                num = Math.floor(num / 10);
+            }
+            return res;
+        }
+        return sumOfDigits(2123456);
+    },
+    "2019-07-27" () {
+
+        //  2019-07-27 计算字符串中某个字符的次数
+
+        const chatNum = (string, chat) => string.match(new RegExp(chat, 'gm')).length;
+
+        return chatNum('abcdefgabcdab', 'b');
+    },
+    "2019-07-28" () {
+        // 2019-07-28 求一个数组的最大奇数和最小偶数
+
+        //  the first solution
+        const maxOddAndminEven0 = arr => {
+
+            let maxOdd = - Infinity, minEven = Infinity;
+
+            arr.forEach(item => {
+                item % 2 ? (maxOdd = maxOdd > item ? maxOdd : item) : (minEven = minEven < item ? minEven : item);
+            });
+
+            return [maxOdd, minEven];
+        }
+        console.log(maxOddAndminEven0([1,2,3,4,5,6,7,8,9,8,12]));
+
+
+        //  the second solution
+        const maxOddAndminEven = arr => {
+
+            let arrOdd = arr.filter(item => item % 2),
+                arrEven = arr.filter(item => item % 2 === 0);
+            
+            return [Math.max.apply(-Infinity, arrOdd), Math.min.apply(Infinity, arrEven)];
+        }
+        return maxOddAndminEven([1,2,3,4,5,6,7,8,9,8,12]);
+    },
+    "2019-07-29" () {
+
+        //  2019-07-29  获取某一时间范围内的闰年，比如1000年到2020年的闰年，以数组的形式输出
+
+        class leapYear {
+
+            constructor (minYear, maxYear) {
+
+                this.minYear = minYear;
+                this.maxYear = maxYear;
+                this.res = [];
+                this.init();
+            }
+            init () {
+
+                for(let i = this.minYear; i <= this.maxYear; i ++) {
+
+                    this.isLeapYear(i) && this.res.push(i);
+                }
+            }
+            isLeapYear(year) {
+                return year % 400 === 0 || ( year % 4 === 0 && year % 100 !== 0 )
+            }
+        }
+
+        return new leapYear(1000, 2020);
+    },
+    "2019-07-30" () {
+
+        //  2019-07-30  打印1000以内能被7整除的数
+
+        const exactDivision = () => {
+
+            let arr = Array.from({length: 1000}, (item, index) => index + 1);
+            return arr.filter(item => !(item % 7));
+        }
+        return exactDivision();
+    },
+    "2019-07-31" () {
+        // 2019-07-31 随机数法求解π
+
+        //  具体的思路是，向一个有内切圆的正方形中扔点，如果落在圆内则计数，落在圆外则不计数
+
+        const getPI = () => {
+
+            //  这里抛点100000次
+            //  中心点与x,与y的距离的平方和小于r的值，则点在圆内
+
+            //  随机生成x,y坐标，这里的r假设为10
+            let res = 0,
+                all = 100000;
+            while(all --) {
+                let x = Math.random() * 20,
+                    y = Math.random() * 20;
+                (Math.abs( 10 - x ) ** 2 + Math.abs(10 - y) ** 2) < 10 ** 2 && res ++
+            }
+            return res / 100000 * 4;
+        }
+        return getPI();
+    }
 }
--- src/2019/2019-08.js
+++ src/2019/2019-08.js
@@ -1,20 +1,155 @@
-export class solution_201908 {
+export const solution_201908 = {
 
-    constructor () {
+    init() {
+        console.log("==================== start 2019-08 =============================");
 
-        for(let i in this.prototype) {
-            console.log(i);
+        for (let i in this) {
+
+            if (i !== 'init') {
+                console.log(this[i]());
+            }
+        }
+    },
+    '2019-08-01' () {
+
+        //  2019-08-01  球的路径
+        //  一球从100米高度自由落下，每次落地后反跳回原高度的一半；再落下。
+        //  求它在 第n次落地时，共经过多少米？第n次反弹多高？
+
+        //  如输入2，则为共经过 100 + 50 + 50米， 第2次的反弹为25米高，输出[200, 25]
+
+        const rebound = num => {
+
+            let height = 100,
+                distance = 0;
+
+            if(num <= 0 || num !== Math.floor(num)) throw '请输入正整数';
+
+            for(let i = 1; i <= num; i ++) {
+
+                height /= 2;
+                distance += 2 * height;
+            }
+            return [distance, height];
+        }
+        return rebound(3);
+    },
+    '2019-08-02' () {
+        // 2019-08-02 任务管理
+        // 一个任务队列中，有任务优先级为高中低三种任务，按高中低的优先级在数组中排序。
+
+        // 后续插入的任务也有高中低，需要将后续插入的任务放到对应的级别末尾，并且不能混乱到该任务队列。
+
+        // 队列的结构如下：
+
+        let arr = [
+            {
+                rank: '高',
+                taskID: '6b8ca06e'
+            },
+            {
+                rank: '高',
+                taskID: '7dac2d24'
+            },
+            {
+                rank: '中',
+                taskID: '668a451a'
+            },
+            {
+                rank: '低',
+                taskID: '6ad40d38'
+            }
+        ]
+        const taskManage = (arr, newTask) => {
+        
+            let rank = ['高', '中', '低'];
+        
+            let rankIndex = rank.indexOf(newTask.rank);
+            
+            let newArr = arr.filter(item => {
+                for(let i = 0; i <= rankIndex; i ++) {
+                    if(item.rank === rank[i]) {
+                        return item.rank;
+                    }
+                }
+            });
+            arr = [...newArr, newTask, ...arr.splice(newArr.length)];
+            return arr;
+        }
+        
+        return taskManage(arr, {
+            rank: '高',
+            taskID: '128a451a'
+        });
+    },
+    '2019-08-03' () {
+        //  2019-08-03  找到某个字符串中指定的字符的所有index索引（下标）
+
+        const getChatIndex = (str, chat) => {
+
+            let res = [];
+
+            str.split('').map((item, index) => {
+                if(item === chat) {
+                    res.push(index);
+                }
+            });
+            return res;
+        }
+        return getChatIndex('abcdefga', 'a');
+    },
+    '2019-08-04' () {
+
+        //  2019-08-04  若干天后是星期几
+        //  已知今天是星期一，求100天后是星期几
+        //  能拓展到今天是星期几不限，未来几天也不限更好
+
+        const futureDay = (today, future) => {
+
+            let week = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+
+            return week[( future % 7 + week.indexOf(today) ) % 7];
         }
 
-    }
-    first () {
+        return futureDay('星期一', '13');
+    },
+    '2019-08-05' () {
 
-        console.log(1);
-    }
-    second () {
-        
+        // 2019-08-05  合并多个有序数组并去重，合并数组从小到大排序
+
+        const packedArray = arr => {
+            
+            let packedArr = arr.reduce((acc, cur) => [...acc, ...cur]);
+
+            return Array.from(new Set(packedArr)).sort((a, b) => a - b);
+        };
+
+        return packedArray([[1, 2], [0, 3, 5], [-1, 4]]);
+    },
+    '2019-08-06' () {
+
+        return '2019-08-06';
+    },
+    '2019-08-07' () {
+
+        return '2019-08-07';
+    },
+    '2019-08-08' () {
+
+        return '2019-08-08';
+    },
+    '2019-08-31' () {
+        //  2019-08-31：sku算法———多维属性状态判断
+        //  算法简化：假设只有3种状态：1、颜色：红蓝灰。2、尺码、大中小。型号、ABC。
+        //  此时库存只有以下数据源：
+        // [
+        //     { "颜色": "红", "尺码": "大", "型号": "A", "skuId": "3158054" },
+        //     { "颜色": "白", "尺码": "中", "型号": "B", "skuId": "3133859" },
+        //     { "颜色": "蓝", "尺码": "小", "型号": "C", "skuId": "3516833" }
+        //  ]
+        // 数据源不可选时将选项按钮置灰并禁用点击，请写出该SKU算法。
         console.log(2);
     }
-    
-
+        //  DFS
+        //  BFS
 }
